ac89be115b62d6239b1b8ae8ade672976eeac330
lepLinus <49990732+lepLinus@users.noreply.github.com>
2023-05-15 21:52:18 +0300

    Add Project File Reader

---
+++ SCAuditStudio/Classes/ProjectFile/ProjectFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCAuditStudio.Classes.ProjectFile
+{
+    public class ProjectFile
+    {
+        public DateTime LastTimeOpened;
+        public DateTime FistTimeOpened;
+        public string Name;
+        public string path;
+    }
+}
---
+++ SCAuditStudio/Classes/ProjectFile/ProjectFileReader.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCAuditStudio.Classes.ProjectFile
+{
+    static class ProjectFileReader
+    {
+        public static ProjectFile[] ReadProjects(string ProjectFilePath)
+        {
+            using (StreamReader r = new StreamReader("ProjectFilePath.json"))
+            {
+                string json = r.ReadToEnd();
+                List<ProjectFile>? projects = JsonConvert.DeserializeObject<List<ProjectFile>>(json);
+                if (projects == null) return Array.Empty<ProjectFile>();
+                return projects.ToArray();
+            }
+        }
+
+        public static void CreateProjectFile(List<ProjectFile> projectsData, string ProjectFilePath)
+        {
+            //open file stream
+            using (StreamWriter file = File.CreateText(ProjectFilePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                //serialize object directly into file stream
+                serializer.Serialize(file, projectsData);
+            }
+        }
+    }
+}
